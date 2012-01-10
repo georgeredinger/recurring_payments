0cbbab3fd25e0bd207d678a15afd38de8d62a33e
George Redinger <george.redinger@radsoft.com>
2012-01-10 10:23:54 -0800

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecurringPayments::Application.config.secret_token = '61ba6c8af339d1de80c872cf592ce567559888068cab32b25f43597bef312d4c221b395cddf738ff43eca240ffba7a9ccc5ed1868082b920324dcde69a32743f'
